48861d35d889fe6adda0fcd8422d3c48295abd52
Dave Steadman <davesteadman@me.com>
2023-04-30 17:18:19 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,84 @@
+﻿// Code under: https://creativecommons.org/publicdomain/zero/1.0/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using TinyMarkup;
+
+/*
+    Expected output:
+
+        Serialized tree:
+
+        [[Root]
+          [[Child1]
+            [[Leaf1] A + b = c]
+            [[Leaf2]general statement]
+          ]
+          [[Child2]
+            [[Child3]
+              [[Leaf3]'string literal']
+            ]
+          ]
+        ]
+
+        Deserialized tree structure:
+
+        [[Root]
+          [[Child1]
+            [[Leaf1]A + b = c]
+            [[Leaf2]general statement]
+          ]
+          [[Child2]
+            [[Child3]
+              [[Leaf3]'string literal']
+            ]
+          ]
+        ]
+
+        Found nested leaf data item: 'string literal'
+*/
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Create a tree structure
+        TMNode rootNode = new TMNode("Root");
+        TMNode childNode1 = rootNode.CreateChildNode("Child1");
+        TMNode childNode2 = rootNode.CreateChildNode("Child2");
+
+        childNode1.CreateChildLeaf("Leaf1", " A + b = c");
+        childNode1.CreateChildLeaf("Leaf2", "general statement");
+        TMNode childNode3 = childNode2.CreateChildNode("Child3");
+        childNode3.CreateChildLeaf("Leaf3", "'string literal'");
+
+        // Serialize the tree structure
+        string serializedTree = TMSerializer.Serialize(rootNode);
+        Console.WriteLine("Serialized tree:");
+        Console.WriteLine(serializedTree);
+
+        // Deserialize the tree structure
+        TMElement deserializedTree = TMParser.Parse(serializedTree);
+        Console.WriteLine("\nDeserialized tree structure:");
+
+        // Serialize the deserialized tree to verify deserialization
+        string reserializedTree = TMSerializer.Serialize(deserializedTree);
+        Console.WriteLine(reserializedTree);
+
+        // Find a nested leaf data item
+        if (deserializedTree is TMNode deserializedRootNode)
+        {
+            string leafData = deserializedRootNode.FindLeafData("Leaf3");
+            Console.WriteLine($"\nFound nested leaf data item: {leafData}");
+        }
+        else
+        {
+            Console.WriteLine("The deserialized tree is not a node.");
+        }
+        Console.ReadKey();
+    }
+}
---
+++ TinyMarkup.cs
@@ -0,0 +1,193 @@
+﻿// Code under: https://creativecommons.org/publicdomain/zero/1.0/
+
+using System.IO;
+using System.Text;
+using System.Collections.Generic;
+
+// TinyMarkup is a simple markup language that is easy parse and serialize.
+// Contained in one file, it is easy to understand and add to a project.
+// The syntax is similar to JSON, without the additional type information. 
+
+namespace TinyMarkup
+{
+    public class TMConsts
+    {
+        public const char openBracketChar = '[';
+        public const char closeBracketChar = ']';
+    }
+
+    public class TMElement
+    {
+        public string Name { get; set; }
+        public TMElement(string initName) => Name = initName;
+    }
+
+    public class TMLeaf : TMElement
+    {
+        public string Data { get; set; }
+
+        public TMLeaf(string newName, string newData) : base(newName) => Data = newData;
+    }
+
+
+    public class TMNode : TMElement
+    {
+        private List<TMElement> elementList;
+
+        public TMNode(string newName) : base(newName) => elementList = new List<TMElement>();
+
+        public int ElementCount() { return elementList.Count; }
+
+        public IReadOnlyList<TMElement> ElementList() { return elementList.AsReadOnly(); }
+
+        public TMNode CreateChildNode(string newNodeName)
+        {
+            TMNode newNode = new TMNode(newNodeName);
+            elementList.Add(newNode);
+            return newNode;
+        }
+
+        public TMLeaf CreateChildLeaf(string newLeafName, string newData)
+        {
+            TMLeaf newLeaf = new TMLeaf(newLeafName, newData);
+            elementList.Add(newLeaf);
+            return newLeaf;
+        }
+
+        public void AddChild(TMElement newChild) { elementList.Add(newChild); }
+
+        public string FindLeafData(string leafName)
+        {
+            return FindLeafDataRecursive(this, leafName);
+        }
+
+        private string FindLeafDataRecursive(TMNode node, string leafName)
+        {
+            foreach (TMElement element in node.elementList)
+            {
+                if ((element is TMLeaf leaf) && (leaf.Name == leafName))
+                    return leaf.Data;
+                else if (element is TMNode childNode)
+                {
+                    string result = FindLeafDataRecursive(childNode, leafName);
+                    if (result != string.Empty)
+                        return result;
+                }
+            }
+            return string.Empty;
+        }
+    }
+
+    public class TMParser
+    {
+        public static TMElement Parse(string input)
+        {
+            using (StringReader reader = new StringReader(input))
+            {
+                return ReadTMElement(reader);
+            }
+        }
+
+        private static TMElement ReadTMElement(StringReader reader)
+        {
+            if (reader.Peek() == -1)
+                return null;
+
+            string name = ReadName(reader);
+
+            SkipWhiteSpace(reader);
+            if (reader.Peek() == TMConsts.openBracketChar)
+            {
+                TMNode node = new TMNode(name);
+                while (reader.Peek() != TMConsts.closeBracketChar)
+                {
+                    TMElement child = ReadTMElement(reader);
+                    if (child != null)
+                        node.AddChild(child);
+                    SkipWhiteSpace(reader); // Add this line to skip white spaces between elements
+                }
+
+                reader.Read(); // Consume '}'
+                return node;
+            }
+            else
+            {
+                string content = ReadContent(reader);
+                reader.Read(); // Consume '}'
+                return new TMLeaf(name, content);
+            }
+        }
+
+        private static void SkipWhiteSpace(StringReader reader)
+        {
+            while (char.IsWhiteSpace((char)reader.Peek()))
+            {
+                reader.Read();
+            }
+        }
+
+        private static string ReadName(StringReader reader)
+        {
+            SkipWhiteSpace(reader);
+            if (reader.Peek() != TMConsts.openBracketChar) return null;
+            reader.Read(); // Consume first '{'
+            SkipWhiteSpace(reader);
+            if (reader.Peek() != TMConsts.openBracketChar) return null;
+            reader.Read(); // Consume second '{'
+
+            SkipWhiteSpace(reader);
+            string name = ReadUntil(reader, TMConsts.closeBracketChar);
+            reader.Read(); // Consume '}'
+
+            return name;
+        }
+
+        private static string ReadContent(StringReader reader)
+        {
+            string newContent = ReadUntil(reader, TMConsts.closeBracketChar);
+            return newContent;
+        }
+
+        private static string ReadUntil(StringReader reader, char endChar)
+        {
+            StringBuilder result = new StringBuilder();
+            while (reader.Peek() != endChar && reader.Peek() != -1)
+            {
+                result.Append((char)reader.Read());
+            }
+            return result.ToString();
+        }
+    }
+
+    public class TMSerializer
+    {
+        public static string Serialize(TMElement element)
+        {
+            StringBuilder sb = new StringBuilder();
+            SerializeCore(sb, element, 0);
+            return sb.ToString();
+        }
+
+        private static void SerializeCore(StringBuilder sb, TMElement element, int indent)
+        {
+            if (indent > 0) sb.Append('\n'); // avoid newline on first output
+            sb.Append(' ', indent * 2);      // 2 spaces per indent level
+
+            if (element is TMNode node)
+            {
+                sb.Append($"{TMConsts.openBracketChar}{TMConsts.openBracketChar}{node.Name}{TMConsts.closeBracketChar}");
+                foreach (TMElement child in node.ElementList())
+                {
+                    SerializeCore(sb, child, indent + 1);
+                }
+                sb.Append('\n');
+                sb.Append(' ', indent * 2); // 2 spaces per indent level
+                sb.Append(TMConsts.closeBracketChar);
+            }
+            else if (element is TMLeaf leaf)
+            {
+                sb.Append($"{TMConsts.openBracketChar}{TMConsts.openBracketChar}{leaf.Name}{TMConsts.closeBracketChar}{leaf.Data}{TMConsts.closeBracketChar}");
+            }
+        }
+    } // class
+} // namespace
